655d794c7ae17a73f32fe1491dad97ebfea73405
webhoster45 <gbengaayoolawale@gmail.com>
2026-03-26 20:31:26 +0100

    Revert "donations: add verify endpoint and client helper"
    This reverts commit e8ac3f14c96a4f180b0b4369c33daf22df76d0b3.

--- backend/src/routes/donation.js
+++ backend/src/routes/donation.js
@@ -7,7 +7,6 @@ const {
   initiateDonation,
   initiateDonationTest,
   verifyDonationTest,
-  verifyDonation,
   getReceipt
 } = require("../controllers/donationController");
 
@@ -15,7 +14,6 @@ const router = express.Router();
 
 router.post("/process", auth, asyncHandler(processDonation));
 router.post("/initiate", auth, asyncHandler(initiateDonation));
-router.post("/verify", auth, asyncHandler(verifyDonation));
 router.post("/initiate-test", asyncHandler(initiateDonationTest));
 router.post("/verify-test", asyncHandler(verifyDonationTest));
 router.get("/receipt/:donationId", requireValidId("donationId"), asyncHandler(getReceipt));
